035f54b2f1f4b3b933c2d6938102e34877f36933
CoreCastric <corentin.castric@gmail.com>
2017-12-01 15:43:12 +0100

    Test de déplacement en mode "Twin Stick Shooter"
    - Ajout d'un script "PlayerMove" pour le déplacement
    
    -Ajout d'un script "FaceMouse" pour que le joueur fasse face à la position de la souris

---
+++ Cactus/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    private Rigidbody2D body2d;
+    public float speed;
+
+	
+	void Start ()
+    {
+        body2d = GetComponent<Rigidbody2D>();
+    
+	}
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        body2d.velocity = new Vector2(horizontal * speed, vertical * speed);
+
+    }
+
+    void Update () {
+		
+	}
+}
